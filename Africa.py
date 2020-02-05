28bebf970bcfe33b5e74e04f2261d00d847af574
Nathan-Binkley <nbink2@gmail.com>
2020-02-05 13:52:19 -0500

    Attempt 1.
    Now plays music

--- Africa.py
+++ Africa.py
@@ -20,6 +20,7 @@ RED = (255, 0, 0)
  
 pygame.mixer.init() 
 pygame.mixer.music.load("TotoAfrica.mp3")
+pygame.mixer.set_num_channels(274)
 pygame.mixer.music.play(0)
 pygame.mixer.music.set_volume(.05)
 # Set the width and height of the screen [width, height]
@@ -27,12 +28,14 @@ size = (150, 200)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
- 
+start_time = time.clock()
 # Loop until the user clicks the close button.
 done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+timer=0
+channel = 0
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -50,6 +53,12 @@ while not done:
     # If you want a background image, replace this clear with blit'ing the
     # background image.
     screen.fill(BLACK)
+    
+    if pygame.time.get_ticks()-timer > 1000:
+        timer = pg.time.get_ticks()
+        pygame.mixer.Channel(channel).play(pygame.mixer.music("TotoAfrica.mp3"))
+        channel += 1
+
  
     # --- Drawing code should go here
  
